b7c6282735c97b407d0391b18c3d4d21ecc94d63
Alisher <zumanovaliser814@gmail.com>
2024-11-04 15:09:46 +0600

    change configs, delete icons, change icon, delete files, create cursor style

---
+++ src/components/UI/cursor/index.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { useEffect } from "react";
+
+const Cursor = () => {
+  useEffect(() => {
+    const cursorLight = document.createElement("div");
+
+    cursorLight.classList.add("cursor");
+    document.body.appendChild(cursorLight);
+
+    const handleMouseMove = (e: MouseEvent) => {
+      cursorLight.style.background = `radial-gradient(circle 800px at ${e.clientX}px ${e.clientY}px, rgba(29, 78, 216, 0.15), transparent 85%)`;
+    };
+
+    document.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+      document.body.removeChild(cursorLight);
+    };
+  }, []);
+
+  return null;
+};
+
+export default Cursor;
